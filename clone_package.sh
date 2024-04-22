61dffff80cf9f28003bd4b8b51f4edc599fcbd74
Benjamin Land <benland100@gmail.com>
2024-04-21 20:14:32 -0400

    Fix superslicer as main repo has been broken for a while

--- clone_package.sh
+++ clone_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pkg=$1
-if [ ! -e /usr/portage/$pkg ]; then
+if [ ! -e /var/db/repos/gentoo/$pkg ]; then
     echo Missing in portage: $pkg
     exit 1
 fi
 mkdir -p /usr/local/portage/$pkg
-cp -r /usr/portage/$pkg/* /usr/local/portage/$pkg 
+cp -r /var/db/repos/gentoo/$pkg/* /usr/local/portage/$pkg 
